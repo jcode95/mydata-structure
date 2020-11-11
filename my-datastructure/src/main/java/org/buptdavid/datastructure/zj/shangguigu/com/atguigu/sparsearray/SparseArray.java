11dc0a0aad2ab96bf6958098498cd637bb7b2ea7
周杰 <597690494@qq.com>
2020-11-11 10:08:14 +0800

    1

--- my-datastructure/src/main/java/org/buptdavid/datastructure/zj/shangguigu/com/atguigu/sparsearray/SparseArray.java
+++ my-datastructure/src/main/java/org/buptdavid/datastructure/zj/shangguigu/com/atguigu/sparsearray/SparseArray.java
@@ -16,27 +16,28 @@ public class SparseArray {
         chessArr1[2][4] = 2;
         for (int[] row : chessArr1) {
             for (int data : row) {
-                System.out.print(data+" ");
+                System.out.print(data + " ");
             }
             System.out.println();
         }
         int row = 0;
+        int flg = 0;//源数组不要放进稀疏数组里面的值
         for (int i = 0; i < chessArr1.length; i++) {
             for (int j = 0; j < chessArr1[i].length; j++) {
-                if (chessArr1[i][j] != 0) {
+                if (chessArr1[i][j] != flg) {
                     row++;
                 }
             }
         }
         //创建稀疏数组
-        int[][] sparseArr = new int[row + 1][3];//第一行记录的是原数组总共有多少列多少行，有几个要记录的值
+        int[][] sparseArr = new int[row + 1][3];//第一行记录的是原数组总共有多少列多少行，标记值
         sparseArr[0][0] = chessArr1.length;
         sparseArr[0][1] = chessArr1[0].length;
-        sparseArr[0][2] = row;
+        sparseArr[0][2] = flg;//源数组不要放进稀疏数组里面的值
         int count = 1;//从第二行开始填充稀疏数组
         for (int i = 0; i < chessArr1.length; i++) {
             for (int j = 0; j < chessArr1[i].length; j++) {
-                if (chessArr1[i][j] != 0) {
+                if (chessArr1[i][j] != flg) {
                     sparseArr[count][0] = i;//行
                     sparseArr[count][1] = j;//列
                     sparseArr[count][2] = chessArr1[i][j];//值
@@ -47,7 +48,28 @@ public class SparseArray {
         System.out.println("-------------------稀疏数组如下---------------------");
         for (int[] rows : sparseArr) {
             for (int data : rows) {
-                System.out.print(data+" ");
+                System.out.print(data + " ");
+            }
+            System.out.println();
+        }
+
+
+        /*使用稀疏数组还原源数组*/
+        int[] first = sparseArr[0];
+        int[][] sourceArr = new int[first[0]][first[1]];
+        for (int i = 0; i < sourceArr.length; i++) {
+            for (int j = 0; j < sourceArr[i].length; j++) {
+                sourceArr[i][j] = first[2];
+            }
+        }
+        for (int i = 1; i < sparseArr.length; i++) {
+            int[] var = sparseArr[i];
+            sourceArr[var[0]][var[1]]=var[2];
+        }
+        System.out.println("-------------------稀疏数组还原的源数组如下---------------------");
+        for (int i = 0; i < sourceArr.length; i++) {
+            for (int j = 0; j < sourceArr[i].length; j++) {
+                System.out.print(sourceArr[i][j]+" ");
             }
             System.out.println();
         }
