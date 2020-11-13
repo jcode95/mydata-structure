c617230fe55c81e8ac90144cd4b0d1c92b384fca
周杰 <597690494@qq.com>
2020-11-13 16:10:29 +0800

    1

--- my-datastructure/src/main/java/org/buptdavid/datastructure/zj/shangguigu/com/atguigu/search/InsertValueSearch.java
+++ my-datastructure/src/main/java/org/buptdavid/datastructure/zj/shangguigu/com/atguigu/search/InsertValueSearch.java
@@ -14,13 +14,12 @@ public class InsertValueSearch implements ISearch {
     }
 
     public int insertValue(int[] arr, int findValue, int low, int hight) {
-        System.out.println("hello");
+//        System.out.println("hello");
         if (low > hight || findValue < arr[0] || findValue > arr[arr.length - 1]) {// findValue < arr[0] || findValue > arr[arr.length-1]提前退出，没有必要再执行了，提升效率
             return -1;
         }
 //        int mid = low + (hight - low) * (1/2);这是二分查找求mid的公式
-        int mid = low + (hight - low) * ((
-                -arr[low]) / (arr[hight] - arr[low]));
+        int mid = low + (hight - low) * ((findValue-arr[low]) / (arr[hight] - arr[low]));
         if (findValue > arr[mid]) {
             return insertValue(arr, findValue, mid + 1, hight);
         } else if (findValue < arr[mid]) {
@@ -33,7 +32,7 @@ public class InsertValueSearch implements ISearch {
     public static void main(String[] args) {
         int[] arr = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
 //        int[] arr = new int[]{-56, 1, 1, 2, 2, 2, 20, 25, 44, 47, 50, 56, 90};
-        int i = new InsertValueSearch().search(arr, 2);
+        int i = new InsertValueSearch().search(arr, 8);
         System.out.println(i);
     }
 }
