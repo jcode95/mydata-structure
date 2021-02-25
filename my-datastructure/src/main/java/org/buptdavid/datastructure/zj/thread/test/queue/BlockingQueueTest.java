8594d226a3c622e904c9d8a375593f3cbef4ff7a
周杰 <597690494@qq.com>
2021-02-25 21:37:09 +0800

    1

---
+++ my-datastructure/src/main/java/org/buptdavid/datastructure/zj/thread/test/queue/BlockingQueueTest.java
@@ -0,0 +1,40 @@
+package org.buptdavid.datastructure.zj.thread.test.queue;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.LinkedTransferQueue;
+
+/**
+ * @author jiezhou
+ * @CalssName: BlockingQueueTest
+ * @Package org.buptdavid.datastructure.zj.thread.test.queue
+ * @Description: 阻塞队列测试（有界/无界）
+ * @date 2021/2/25/21:15
+ */
+public class BlockingQueueTest {
+    public static void main(String[] args) {
+        ArrayBlockingQueue<Integer> arrayBlockingQueue = new ArrayBlockingQueue<>(3);//基于数组的有界阻塞队列，如果放入的元素个数大于临界值，抛出异常 java.lang.IllegalStateException: Queue full
+        for (int i = 0; i < 3; i++) {
+            arrayBlockingQueue.add(i);
+        }
+        //打印
+        arrayBlockingQueue.forEach(e -> System.out.println("e = " + e));
+
+
+        LinkedBlockingQueue<Integer> linkedBlockingQueue = new LinkedBlockingQueue<>(3);//基于链表的有界阻塞队列，如果不指定临界值，默认是 Integer.MAX_VALUE,如果元素个数超过临界值，抛出异常java.lang.IllegalStateException: Queue full
+        for (int i = 0; i < 3; i++) {
+            linkedBlockingQueue.add(i);
+        }
+        //打印
+        linkedBlockingQueue.forEach(e -> System.out.println("e = " + e));
+
+        LinkedTransferQueue<Integer> linkedTransferQueue = new LinkedTransferQueue<Integer>();//基于链表的无界阻塞队列
+        for (int i = 0; i < 4; i++) {
+            linkedTransferQueue.add(i);
+        }
+        linkedTransferQueue.forEach(e -> System.out.println("e = " + e));
+    }
+
+
+}
+
