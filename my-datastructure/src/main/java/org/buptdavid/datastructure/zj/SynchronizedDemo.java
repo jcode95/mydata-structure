3bdec9014313d62eb306ef464d829b7f675bf4c7
周杰 <597690494@qq.com>
2021-04-24 21:24:01 +0800

    synchronized 关键字 作用在不同地方的详细介绍

--- my-datastructure/src/main/java/org/buptdavid/datastructure/zj/SynchronizedDemo.java
+++ my-datastructure/src/main/java/org/buptdavid/datastructure/zj/SynchronizedDemo.java
@@ -37,6 +37,82 @@ public class SynchronizedDemo {
      Lock可以提高多个线程进行读操作的效率。（可以通过readwritelock实现读写分离）
      在性能上来说，如果竞争资源不激烈，两者的性能是差不多的，而当竞争资源非常激烈时（即有大量线程同时竞争），此时Lock的性能要远远优于synchronized。所以说，在具体使用时要根据适当情况选择。
      synchronized使用Object对象本身的wait 、notify、notifyAll调度机制，而Lock可以使用Condition进行线程之间的调度，
+     *
+     *
+     * 特别注意：
+     *  电魂题： synchronized 修饰静态方法、普通方法与代码块的区别？
+     *  ➤ synchronized修饰普通方法（对象锁）
+     *      ✍  结论：修饰普通方法的时候，锁的是当前对象。如果创建个新对象b2，则不能保持同步！！
+     *
+     *  ➤ synchronized修饰静态方法（类锁）
+     *      ✍  结论：就算两个不同的对象，也可以保持同步！！！
+     *
+     *  ➤ synchronized修饰代码块（也分 类锁、对象锁）
+     *      类锁  如下
+     *          class B{
+                     *  public void mB(String value) {
+                     *         synchronized(B.class){
+                     *             for (int i = 0; i < 10; i++) {
+                     *                 System.out.println(value);
+                     *                 try{
+                     *                     Thread.sleep(150);
+                     *                 }catch(Exception e){
+                     *
+                     *                 }
+                     *             }
+                     *         }
+                     *
+                     *     }
+                     *  public void mC(String value) {
+                     *         synchronized(B.class){
+                     *             for (int i = 0; i < 10; i++) {
+                     *                 System.out.println(value);
+                     *                 try{
+                     *                     Thread.sleep(150);
+                     *                 }catch(Exception e){
+                     *
+                     *                 }
+                     *             }
+                     *         }
+                     *     }
+                     * }
+     *  上面是类锁：所有对象可以保持同步。
+     *      对象锁  如下
+     *          class B{
+                     *  public void mB(String value) {
+                     *         synchronized(this){
+                     *             for (int i = 0; i < 10; i++) {
+                     *                 System.out.println(value);
+                     *                 try{
+                     *                     Thread.sleep(150);
+                     *                 }catch(Exception e){
+                     *
+                     *                 }
+                     *             }
+                     *         }
+                     *
+                     *     }
+                     *  public void mC(String value) {
+                     *         synchronized(this){
+                     *             for (int i = 0; i < 10; i++) {
+                     *                 System.out.println(value);
+                     *                 try{
+                     *                     Thread.sleep(150);
+                     *                 }catch(Exception e){
+                     *
+                     *                 }
+                     *             }
+                     *         }
+                     *     }
+                     * }
+     *
+     *      上面是对象锁：同一对象可以保持同步，不同对象不可保持同步。
+     *
+     *
+     *
+     *
+     *
+     *
      * */
 
 
