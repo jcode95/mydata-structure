38268d6035016afa40d4999c50b838a21e6df531
周杰 <597690494@qq.com>
2021-05-17 15:47:21 +0800

    JMX

--- my-datastructure/src/main/java/org/buptdavid/datastructure/zj/JMX/HelloAgent.java
+++ my-datastructure/src/main/java/org/buptdavid/datastructure/zj/JMX/HelloAgent.java
@@ -25,4 +25,27 @@ public class HelloAgent {
         mBeanServer.registerMBean(adaptorServer,adapterName);
         adaptorServer.start();
     }
+
+
+
+
+    /*
+    * 1、linux下利用JMX监控Tomcat
+　　利用JMX监控Tomcat，就是相当于部署在tomcat上的应用作为服务端，也就是被管理资源的对象。然后通过程序或者jconsole远程连接到该应用上来。远程连接需要服务器端提供ip和port。如果需要加密访问的话，还需要配置用户名、密码等参数。
+    CATALINA_ OPTS="*$CATALINA_ OPTS -Dcom.sun.management.jmxremote -Dava.misever.hostname=192.168.1.2 -Dcom.sun.management.jmxremote. port=8888 -Dcom.sun.management.jmxremote ssl=false -Dcom.sun.management.jmxremote.authenticate=false"
+
+    主要是在tomcat下的文件catalina.sh中进行一些环境变量的配置配置：
+    -Dcom.sun.management.jmxremote=true                 相关 JMX 代理侦听开关
+
+    -Djava.rmi.server.hostname                                     服务器端的IP
+    -Dcom.sun.management.jmxremote.port=29094             相关 JMX 代理侦听请求的端口
+
+    -Dcom.sun.management.jmxremote.ssl=false              指定是否使用 SSL 通讯
+
+    -Dcom.sun.management.jmxremote.authenticate=false     指定是否需要密码验证
+
+    这样就可以通过客户端或者jconsole对tomcat进行监控。
+    *
+    *
+    * */
 }
