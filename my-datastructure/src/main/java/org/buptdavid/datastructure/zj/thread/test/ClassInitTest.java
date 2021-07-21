eae5ac91857a3f05c9cd4e51eb608f885aed2e56
周杰 <597690494@qq.com>
2021-07-21 19:59:34 +0800

    静态代码块出现在声明变量语句前面 值是啥 的 测试

---
+++ my-datastructure/src/main/java/org/buptdavid/datastructure/zj/thread/test/ClassInitTest.java
@@ -0,0 +1,30 @@
+package org.buptdavid.datastructure.zj.thread.test;
+
+public class ClassInitTest {
+   private static int num = 1;
+
+   static{
+       num = 2;
+       number = 20;
+       System.out.println(num);
+       //System.out.println(number);//报错：非法的前向引用。
+   }
+
+   /**
+    * 1、linking之prepare: number = 0 --> initial: 20 --> 10
+    * 2、这里因为静态代码块出现在声明变量语句前面，所以之前被准备阶段为0的number变量会
+    * 首先被初始化为20，再接着被初始化成10（这也是面试时常考的问题哦）
+    *
+    */
+   private static int number = 10;
+
+    public static void main(String[] args) {
+        System.out.println(ClassInitTest.num);//2
+        System.out.println(ClassInitTest.number);//10
+    }
+    int anInt=0;
+
+    public ClassInitTest(int anInt) {
+        this.anInt = anInt;
+    }
+}
