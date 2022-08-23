454f357a65959b7db58a68d2eb4f07af20f51034
zhoujie <597690494@qq.com>
2022-08-23 20:29:27 +0800

    位运算 的骚操作

--- my-datastructure/src/main/java/org/buptdavid/datastructure/zj/zuo_shen/位运算/剑指offer65.java
+++ my-datastructure/src/main/java/org/buptdavid/datastructure/zj/zuo_shen/位运算/剑指offer65.java
@@ -16,30 +16,34 @@ public class 剑指offer65 {
 
         int substract = substract(a, b);
         System.out.println("substract = " + substract);
+
+        System.out.println("ces = " + (-8 >> 2));
+        System.out.println("ces1 = " + (6 & 2));
+        System.out.println("process(6) = " + process(6));
     }
 
     /**
      * 1）位运算加法：
-     *
-     *
+     * <p>
+     * <p>
      * 对于两数二进制相加，不进位相加相当于异或num，指考虑进位计算为与，
      * 并左移一位carry;当carry不为0时表示有进位，将进位的数carry不进位“加”到num中，得到新的num和carry；
      */
     public static int add(int num1, int num2) {
-        if(num2 == 0)
+        if (num2 == 0)
             return num1;
         int sum = num1 ^ num2;
-        int carry = (num1 & num2) << 1;
+        int carry = (num1 & num2) << 1;//你也可以理解为x乘以2的n次方。n是左移的位数，这里就是1。
         return add(sum, carry);
     }
 
     /**
      * num1: 减数
      * num2: 被减数
-     *
-     *
+     * <p>
+     * <p>
      * 通过加法器来实现减法器，即首先实现一个加法器，设为add(int a,int b)；
-     *
+     * <p>
      * 然后对两个数num1，num2，num1-num2=num1+(-num2)；
      * 对于一个数的负数在计算机中时取反再加1，即m_num2=add(~num2,1)；然后add(num1,m_num2);
      */
@@ -48,4 +52,30 @@ public class 剑指offer65 {
         int result = add(num1, subtractor); // add()即上述加法运算　　
         return result;
     }
+
+    /**
+     * 求平均值，比如有两个int类型变量x、y,首先要求x+y的和，再除以2，但是有可能x+y的结果会超过int的最大表示范围，所以位运算就派上用场啦。
+     * (x&y)+((x^y)>>1);
+     */
+    public static int average(int x, int y) {
+        return (x & y) + ((x ^ y) >> 1);
+    }
+
+    /**
+     * 求一个整数二进制数中1的个数
+     */
+
+    public static int process(int n) {
+        int count = 0;
+        while (n != 0) {
+            // 与1做与运算，结果为1则说明个位数是1，然后移位
+            if ((n & 1) == 1) {
+                count++;
+            }
+            n >>= 1;
+        }
+        System.out.println("1的个数为" + count);
+        return count;
+    }
+
 }
