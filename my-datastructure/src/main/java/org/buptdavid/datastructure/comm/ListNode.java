84fc23756c541be3d07760427c192d479c99daa4
zhoujie <597690494@qq.com>
2023-08-18 18:24:55 +0800

    构建 NodeUtils 工具类

--- my-datastructure/src/main/java/org/buptdavid/datastructure/comm/ListNode.java
+++ my-datastructure/src/main/java/org/buptdavid/datastructure/comm/ListNode.java
@@ -8,10 +8,29 @@ package org.buptdavid.datastructure.comm;
  * @date 2020/8/3/14:07
  */
 public class ListNode {
-    int val;
-    ListNode next;
+    public int val;
+    public ListNode next;
 
     ListNode(int x) {
         val = x;
+    }
+
+    /**
+     * 打印节点
+     */
+    public void print() {
+        ListNode head = this;
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
+        while (head != null) {
+            sb.append(head.val).append(",");
+            head = head.next;
+        }
+        String resultStr = sb.toString();
+        if (sb.lastIndexOf(",") > 0) {
+            resultStr = sb.substring(0, sb.lastIndexOf(","));
+        }
+        resultStr += "]";
+        System.out.println(resultStr);
     }
 }
--- my-datastructure/src/main/java/org/buptdavid/datastructure/comm/NodeUtils.java
+++ my-datastructure/src/main/java/org/buptdavid/datastructure/comm/NodeUtils.java
@@ -1,5 +1,7 @@
 package org.buptdavid.datastructure.comm;
 
+import java.util.Random;
+
 /**
  * @author zhoujie
  * @CalssName: NodeUtils
@@ -7,9 +9,23 @@ package org.buptdavid.datastructure.comm;
  * @Description:
  */
 public class NodeUtils {
+    static Random random = new Random();
 
-
-
+    public static ListNode createNode(int length, int randomMax) {
+        ListNode head = null;
+        ListNode listNode = null;
+        for (int i = 0; i < length; i++) {
+            ListNode node = new ListNode(random.nextInt(randomMax) + 1);
+            if (head == null) {
+                head = node;
+                listNode = head;
+            } else {
+                listNode.next = node;
+                listNode = listNode.next;
+            }
+        }
+        return head;
+    }
 
 
 }
